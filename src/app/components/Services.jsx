a84cdec0189e97ea4f0ad7e02a8322fcf1d4d68c
ifrah-ashraf <ifrahashraf48@gmail.com>
2024-06-12 23:43:14 +0530

    organised the all the images in images folder and video folder seprately

--- src/app/components/Services.jsx
+++ src/app/components/Services.jsx
@@ -3,84 +3,84 @@ import Link from 'next/link'
 function Services() {
   return (
     <>
-    <h1 className='text-3xl font-bold ml-[550px] mt-[180px] absolute'>Care During Pregnancy</h1>
-    <div id='services' className='mt-[250px]'>
-      
-     <div className='flex gap-20 mt-[80px] '>
-     <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
+      <h1 className='text-3xl font-bold ml-[550px] mt-[180px] absolute'>Care During Pregnancy</h1>
+      <div id='services' className='mt-[250px]'>
 
-<div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
+        <div className='flex gap-20 mt-[80px] '>
+          <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
+            <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotify app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
 
-<div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-    </div>
-  </div>
-  <div className='flex gap-20 mt-[80px] '>
-     <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
+          <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
+            <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
 
-<div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
+          <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
+            <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">New album is released!</h2>
+              <p>Click the button to listen on Spotiwhy app.</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Listen</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='flex gap-20 mt-[80px] '>
+        <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
+          <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">New album is released!</h2>
+            <p>Click the button to listen on Spotiwhy app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Listen</button>
+            </div>
+          </div>
+        </div>
 
-<div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-    </div>
- 
-    
-  
-  </>
+        <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
+          <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">New album is released!</h2>
+            <p>Click the button to listen on Spotiwhy app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Listen</button>
+            </div>
+          </div>
+        </div>
 
-   
+        <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
+          <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">New album is released!</h2>
+            <p>Click the button to listen on Spotiwhy app.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Listen</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    </>
+
+
   )
 }
 
