895ecd74183bf90de9c5c0d72d72e92a92e4d466
snehajain11 <snehajain11105@gmail.com>
2024-06-13 01:16:42 +0530

    changes in ui

--- src/app/Services/page.jsx
+++ src/app/Services/page.jsx
@@ -5,82 +5,26 @@ function Services() {
     <>
 
     <h1 className='text-3xl font-bold ml-[550px] mt-[180px] absolute text-orange-500 font-serif'>Care During Pregnancy</h1>
-    <div className=' '>
-    <div id='services' className='mt-[300px] bg-orange-100 h-[1200px] rounded-full'>
+   
+    <div id='services' className='mt-[300px] bg-orange-100 h-[1000px] rounded-tr-full w-[100vw]'>
+     
       <img src="https://i.pinimg.com/originals/5f/68/b1/5f68b18a611acf00b429eca5b8236801.gif" alt="" srcset="" className='ml-[600px] w-[350px] ' />
-     <div className='flex gap-10 mt-[50px] ml-[20px]  '>
-     <div className="card lg:card-side bg-base-100 shadow-xl w-[500px] mt-[20px]">
-  <figure><img src="https://i.pinimg.com/474x/bf/ef/97/bfef97712edce45e6299968f3d6072a8.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">Every bite counts—make it nutritious and beneficial for your baby!</h2>
-    <p>Click the button to get best diet for you.</p>
-    <div className="card-actions justify-end">
-      <Link href="/KnowDiet" className="btn btn-primary">Know Now</Link>
+      <div className='mt-[90px] ml-[20px]'>
+       <Link href="/KnowDiet"><button className='bg-black rounded-xl text-white p-4'>Know Your Diet</button></Link>
+        <p className='mt-4'>"Every bite counts—make it nutritious and beneficial for your baby."</p>
+      </div>
+      <div className='mt-[90px] ml-[20px]'>
+       <Link href="/KnowDiet"><button className='bg-black rounded-xl text-white p-4'>Get Your Daily plan</button></Link>
+        <p className='mt-4'>"Every bite counts—make it nutritious and beneficial for your baby."</p>
+      </div>
+      <div className='mt-[90px] ml-[20px]'>
+       <Link href="/KnowDiet"><button className='bg-black rounded-xl text-white p-4'>Exercise</button></Link>
+        <p className='mt-4'>"Every bite counts—make it nutritious and beneficial for your baby."</p>
+      </div>
+    
     </div>
-  </div>
-</div>
+   
 
-<div className="card lg:card-side bg-base-100 shadow-xl w-[500px] mt-[20px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-
-<div className="card lg:card-side bg-base-100 shadow-xl w-[500px] mt-[20px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-    </div>
-  
-  <div className='flex gap-20 mt-[80px] ml-[20px]'>
-     <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-
-        <div className='flex gap-20 mt-[80px] '>
-          <div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-            <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
-            <div className="card-body">
-              <h2 className="card-title">New album is released!</h2>
-              <p>Click the button to listen on Spotify app.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Listen</button>
-              </div>
-            </div>
-          </div>
-
-<div className="card lg:card-side bg-base-100 shadow-xl w-[500px]">
-  <figure><img src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New album is released!</h2>
-    <p>Click the button to listen on Spotiwhy app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Listen</button>
-    </div>
-  </div>
-</div>
-    </div>
- </div>
-    </div>
-  </div>
   </>
 
   )
