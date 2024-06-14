b7b46d9366f7857dc29dec702f4ea26930ff5f08
MishikaGoyal <mishikaaagarwal@gmail.com>
2024-06-15 01:06:02 +0530

    "Website Name added"

--- frontend/src/app/components/Features.jsx
+++ frontend/src/app/components/Features.jsx
@@ -7,7 +7,7 @@ function Features() {
       <div className='mt-[100px]'>
         <ul className='flex-row md:flex'>
           <li><div className=' w-[300px] md:w-[500px]  ml-[30px] md:ml-[50px] mt-[50px]'>
-          Congratulations on your pregnancy! We are thrilled to accompany you on this incredible journey to parenthood. At[website name], we understand that every pregnancy is unique and filled with moments of joy, anticipation, and sometimes uncertainty. Our mission is to provide you with the support, information, and community you need to navigate this beautiful chapter of your life.
+          Congratulations on your pregnancy! We are thrilled to accompany you on this incredible journey to parenthood. At MummaCare, we understand that every pregnancy is unique and filled with moments of joy, anticipation, and sometimes uncertainty. Our mission is to provide you with the support, information, and community you need to navigate this beautiful chapter of your life.
 
 Whether you're looking for expert advice, health and wellness tips, you'll find it here. Explore our wide range of resources, from personalized diet and to informative articles and machine learning designed to help you detect your posture experience as smooth and enjoyable as possible.
 
